bc63bc8834e8f6415f8ea8e2737460e6d8d8d6de
ababup1192 <ababup1192@gmail.com>
2016-05-23 00:42:02 +0900

    Implement actionCreator

--- src/scripts/actionCreator.ts
+++ src/scripts/actionCreator.ts
@@ -2,7 +2,12 @@ import * as Bacon from "baconjs";
 import {Dispatcher} from "./dispatcher";
 import {Constant} from "./constant";
 import {JValue, JNumber, JString, JBool, JNull, JArray, JField, JObject, JsonAst} from "./jsonAst";
-import {Utils} from "./utils"
+import {Utils} from "./utils";
+
+interface IUpdateObject {
+    id: string;
+    newValue: any;
+}
 
 export class ActionCreator {
     private d: Dispatcher;
@@ -15,10 +20,15 @@ export class ActionCreator {
         this.d.push(Constant.CHANGE_TEXT, text);
     }
 
+    public updateAst(updateObject: IUpdateObject): void {
+        this.d.push(Constant.UPDATE_AST, updateObject);
+    }
+
     public createProperty(initialValue: string): Bacon.Property<JValue, JValue> {
         const json: any = Utils.forceEval<String, JValue>(JSON.parse, initialValue);
-        return Bacon.update<any, any, JValue>(JsonAst.toAst(json),
-            [this.d.stream(Constant.CHANGE_TEXT)], this._changeText
+        return Bacon.update<JValue, string, IUpdateObject, JValue>(JsonAst.toAst(json),
+            [this.d.stream(Constant.CHANGE_TEXT)], this._changeText,
+            [this.d.stream(Constant.UPDATE_AST)], this._updateAst
         );
     }
 
@@ -28,5 +38,10 @@ export class ActionCreator {
         } catch (e) {
             return ast;
         }
+    }
+
+    private _updateAst(ast: JValue, updateObject: IUpdateObject): JValue {
+        const {id, newValue}: IUpdateObject = updateObject;
+        return ast.update(id, newValue);
     }
 }
--- test/actionCreator.test.ts
+++ test/actionCreator.test.ts
@@ -26,4 +26,18 @@ describe("ActioCreator", () => {
             chai.assert.strictEqual(actual.toString(), new JObject([new JField("b", new JNumber(456))]).toString());
         });
     });
+    describe("updateJsonAst", () => {
+        it("should return new JsonAst", () => {
+            const dispatcher: Dispatcher = new Dispatcher();
+            const action: ActionCreator = new ActionCreator(dispatcher);
+            let id: string = "";
+            let actual: JValue = undefined;
+            action.createProperty(`{"a": 123}`).onValue((value: JValue) => {
+                id = ((value as JObject).obj[0].value as JNumber).id;
+                actual = value;
+            });
+            action.updateAst({id: id, newValue: 456});
+            chai.assert.strictEqual(actual.toString(), new JObject([new JField("a", new JNumber(456))]).toString());
+        });
+    });
 });
